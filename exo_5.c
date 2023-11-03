876b86ce745967e2d2658bc6240a99b45fed0759
Maks <maksmunait@gmail.com>
2023-11-03 15:24:25 +0100

    final commit TP6

--- exo_5.c
+++ exo_5.c
@@ -3,102 +3,130 @@
 #include <string.h>
 #define MAX_SIZE 1024  // pow(2, 10)
 
+int len_word(char *word);
 
 int compteLignes(FILE * file);
 
 int compteMots(FILE * file);
 
-int compteCaracteres(FILE * fichier);
+int compteCaracteres(FILE * file);
 
-void countStats(FILE *file, int *charCount, int *wordCount, int *lineCount);
+void compteTout(FILE *file, int *caracteres, int *mots, int *lignes);
 
-int my_wc(FILE * file);
+void affiche_options(char *mode, int *affiche_char, int *affiche_mots, int *affiche_lignes, int *affiche_version);
 
+void affiche_synopsis(int affiche_char, int affiche_mots, int affiche_lignes, int affiche_version, int caracteres, int mots, int lignes);
+
+// int my_wc(FILE * file);
+
+void usage(char *nom);
 
 int main (int argc, char *argv[]){
     FILE* file;
-    int displayCharCount = 0;
-    int displayWordCount = 0;
-    int displayLineCount = 0;
-    int displayVersion = 0;
+    int affiche_char = 0;
+    int affiche_mots = 0;
+    int affiche_lignes = 0;
+    int affiche_version = 0;
 
-    int charCount = 0;
-    int wordCount = 0;
-    int lineCount = 0;
+    int caracteres = 0;
+    int mots = 0;
+    int lignes = 0;
+    char *mode = *(argv + 1);
+
+    // Si on a mis quelque chose après caractère '-'
+    affiche_options(mode, &affiche_char, &affiche_mots, &affiche_lignes, &affiche_version);
     
-    char *mode = *++argv;
-
-    for (int i = 1; *(mode + i) != '\0'; ++i){
-        if (*mode == '-' && strlen(mode) > 1){
-            if (*(mode + i) == 'l') { displayLineCount = 1; }
-            else if (*(mode + i) == 'm') { displayWordCount = 1; }
-            else if (*(mode + i) == 'c') { displayCharCount = 1; }
-            else if (*(mode + i) == 'v') { displayVersion = 1; }
-        }
-    }
-    if (!displayCharCount && !displayWordCount && !displayLineCount){
-        displayCharCount = 1; displayWordCount = 1; displayLineCount = 1;
-    }
-
+    // Si on n'a rien fournie donc on dit par défault: lignes, mots, caractères
+    if (!affiche_char && !affiche_mots && !affiche_lignes) { affiche_char = 1; affiche_mots = 1; affiche_lignes = 1; }
+    
+    // On parcours tous les fichiers
     while (*++argv){
+        if ((*argv)[0] == '-') continue;  // pour éviter l'ouverture des options comme fichier
         file = fopen(*argv, "r");
-        if (file == NULL) { printf("Fichier %s ne peut pas être lu.\n", *argv); }
+
+        if (!file) { printf("Fichier %s ne peut pas être lu.\n\n", *argv); continue;}
         else {
             // printf("Fichier %s peut-être lu.\n", *argv);
-            countStats (file, &charCount, &wordCount, &lineCount);
+            
+            compteTout (file, &caracteres, &mots, &lignes);  // Ici on compte tout: lignes, mots, caractères
+            
+            affiche_synopsis(affiche_char, affiche_mots, affiche_lignes, affiche_version, caracteres, mots, lignes); // On les affiche
 
-            if (displayCharCount) { printf("nombre de caracteres est: %d\n", charCount); }
-            if (displayWordCount) { printf("nombre de mots est: %d\n", wordCount); }
-            if (displayLineCount) { printf("nombre de lignes est: %d\n\n", lineCount); }
-            fclose(file);
+            fclose(file);  // la férmeture du fichier
         }
     }
-    printf("%d, %d, %d, %d\n", displayLineCount, displayWordCount, displayCharCount, displayVersion);
     return 0;
+}
+
+
+int len_word(char *word){  // la longueur du mot
+    int i = 0;
+    while (*(word + i)){ ++i; } return i;
 }
 
 int compteLignes(FILE * file){
     int count = 0;
     char line[MAX_SIZE]; // Buffer for reading lines
-    int c;
+
     while (fgets(line, MAX_SIZE, file)) { count++; }
-    /*while ((c = fgetc(file)) != EOF) if (c == '\n') { count++; }
-    count++;*/
-    
+    /*while ((c = fgetc(file)) != EOF) if (c == '\n') { count++; } count++;*/
     return count;
 }
 
 int compteMots(FILE * file){
     int count = 0;
     char word[MAX_SIZE];
-    
     while (fscanf(file, " %255s ", word) == 1){ ++count; }
     return count;
 }
 
-int compteCaracteres(FILE * fichier){
+int compteCaracteres(FILE * file){
     int res = 0;
     char c;
-    while ((c = fgetc(fichier)) != EOF) { ++res; }
+    while ((c = fgetc(file)) != EOF) { ++res; }
     return res;
 }
 
-void countStats(FILE *file, int *charCount, int *wordCount, int *lineCount) {
+void compteTout(FILE *file, int *caracteres, int *mots, int *lignes) {
     int ch;
     int inWord = 0;
 
-    *charCount = 0; //compteCaracteres(file);
-    *wordCount = 0; //compteMots(file);
-    *lineCount = 0; //compteLignes(file);
+    *caracteres = 0;  //compteCaracteres(file);
+    *mots = 0;        //compteMots(file);
+    *lignes = 0;      //compteLignes(file);
 
     while ((ch = fgetc(file)) != EOF) {
-        if (ch == '\n') { (*lineCount)++; }
+        if (ch == '\n') { (*lignes)++; }
 
         if (ch == ' ' || ch == '\t' || ch == '\n') { inWord = 0; }
         else if (!inWord) {
             inWord = 1;
-            (*wordCount)++; }
-        (*charCount)++;
+            (*mots)++; }
+        (*caracteres)++;
     }
-    (*lineCount)++;
+    (*lignes)++;
+}
+
+void affiche_options(char *mode, int *affiche_char, int *affiche_mots, int *affiche_lignes, int *affiche_version){
+    for (int i = 1; i < len_word(mode); ++i){
+        if (*mode == '-' && strlen(mode) > 1){
+            if (*(mode + i) == 'l') { *affiche_lignes = 1; }
+            else if (*(mode + i) == 'm') { *affiche_mots = 1; }
+            else if (*(mode + i) == 'c') { *affiche_char = 1; }
+            else if (*(mode + i) == 'v') { *affiche_version = 1; }
+            else { usage(mode); printf("Veuillez réessayez!\n"); }
+        }
+    }
+}
+
+void affiche_synopsis(int affiche_char, int affiche_mots, int affiche_lignes, int affiche_version, int caracteres, int mots, int lignes){
+    if (affiche_char) { printf("nombre de caracteres est: %d\n", caracteres); }
+    if (affiche_mots) { printf("nombre de mots est: %d\n", mots); }
+    if (affiche_lignes) { printf("nombre de lignes est: %d\n\n", lignes); }
+    if (affiche_version) { printf("C’est ma version du programme word count.\n"); }
+}
+
+void usage(char *nom){
+    printf("Erreur une option invalide est fournie: %s\n", nom);
+    printf("Après '-' vous ne devez utiliser que ces 4 lettres: 'l', 'm', 'c', 'v'.\n\n");
 }
